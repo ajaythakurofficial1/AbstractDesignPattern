debe6497fcb1ba2d308e803819c48b9172141139
ajaythakurofficial1 <165251043+ajaythakurofficial1@users.noreply.github.com>
2025-04-16 11:46:21 +0530

    Add files via upload

---
+++ WebDeveloper.java
@@ -0,0 +1,16 @@
+package com.nt.AbstractDesignPattern;
+
+public class WebDeveloper implements Employee {
+
+	@Override
+	public int Salary() {
+
+		return 200000;
+	}
+
+	@Override
+	public String name() {
+		return "ajay";
+	}
+
+}
