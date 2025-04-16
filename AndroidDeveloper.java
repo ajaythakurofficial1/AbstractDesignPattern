9be0874fa7c083ab36b971ea3c9a5c05fd98ff41
ajaythakurofficial1 <165251043+ajaythakurofficial1@users.noreply.github.com>
2025-04-16 11:44:41 +0530

    Add files via upload

---
+++ AndroidDeveloper.java
@@ -0,0 +1,17 @@
+package com.nt.AbstractDesignPattern;
+
+public class AndroidDeveloper implements Employee {
+
+	@Override
+	public int Salary() {
+		
+		return 40000;
+	}
+
+	@Override
+	public String name() {
+		
+		return "anushka android developer";
+	}
+
+}
